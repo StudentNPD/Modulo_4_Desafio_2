8071526636da4b79bb355e263215c2ddc72e2bab
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-08-26 11:45:55 -0400

    Update juego.py

--- juego.py
+++ juego.py
@@ -9,3 +9,9 @@ print("\n \n")
 
 hero = personaje.player()
 hero.create_hero()
+
+print(
+    f"\nBienvenido {hero.name}, ha comenzado la gran purga de orcos a comenzado, son una terrible invasión... 
+    Tienen otras costumbres y malos hábitos, han venido a contaminar nuestro reino con las impurezas de su mundo..."
+)
+
